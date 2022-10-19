f68b95299708a315592b577d9e7bda7700508c1a
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-19 14:10:49 -0700

    Initialized project

---
+++ src/main/java/com/evanishyn/ecoursetracker/EcourseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.evanishyn.ecoursetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcourseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcourseTrackerApplication.class, args);
+	}
+
+}
