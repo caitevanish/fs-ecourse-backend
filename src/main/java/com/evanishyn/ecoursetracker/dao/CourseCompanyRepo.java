b6f1f0af17dea5711f0c67adaa78479f26111851
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-19 20:04:43 -0700

    Added Cross Origins for local host 4200

--- src/main/java/com/evanishyn/ecoursetracker/dao/CourseCompanyRepo.java
+++ src/main/java/com/evanishyn/ecoursetracker/dao/CourseCompanyRepo.java
@@ -3,7 +3,9 @@ package com.evanishyn.ecoursetracker.dao;
 import com.evanishyn.ecoursetracker.entity.CourseCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "courseCompany", path="course-company")
 public interface CourseCompanyRepo extends JpaRepository<CourseCompany,Integer> {
 }
--- src/main/java/com/evanishyn/ecoursetracker/dao/CourseRepo.java
+++ src/main/java/com/evanishyn/ecoursetracker/dao/CourseRepo.java
@@ -2,7 +2,9 @@ package com.evanishyn.ecoursetracker.dao;
 
 import com.evanishyn.ecoursetracker.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface CourseRepo extends JpaRepository<Course,Integer> {
 
 }
